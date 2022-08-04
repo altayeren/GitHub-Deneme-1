64012aca0fe3bc50bb166451d4c974bf4b149970
altayeren <altayeren@gmail.com>
2022-08-04 20:15:04 +0300

    main branch degisiklik

--- src/kisiselKullanimDeneme_1/GitHubKullanimiDeneme_1.java
+++ src/kisiselKullanimDeneme_1/GitHubKullanimiDeneme_1.java
@@ -4,6 +4,9 @@ public class GitHubKullanimiDeneme_1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("That is it");
+
         System.out.println("Welcome to Altay branch");
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
     }
 }
