e6d25036099c69a0f7355a81406f79f72337e788
altayeren <altayeren@gmail.com>
2022-08-04 11:09:36 +0300

    ilk değişiklik

--- src/kisiselKullanimDeneme_1/GitHubKullanimiDeneme_1.java
+++ src/kisiselKullanimDeneme_1/GitHubKullanimiDeneme_1.java
@@ -3,5 +3,6 @@ package kisiselKullanimDeneme_1;
 public class GitHubKullanimiDeneme_1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("That is it");
     }
 }
