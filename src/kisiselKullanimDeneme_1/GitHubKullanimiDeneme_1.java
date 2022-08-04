6a59f69e44473f0a8a07dfa1eb05e2e39acc70b7
altayeren <altayeren@gmail.com>
2022-08-04 19:47:50 +0300

    altay branchi güncellendi

--- src/kisiselKullanimDeneme_1/GitHubKullanimiDeneme_1.java
+++ src/kisiselKullanimDeneme_1/GitHubKullanimiDeneme_1.java
@@ -4,5 +4,6 @@ public class GitHubKullanimiDeneme_1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("That is it");
+        System.out.println("Welcome to Altay branch");
     }
 }
---
+++ src/kisiselKullanimDeneme_1/ozellik1.java
@@ -0,0 +1,7 @@
+package kisiselKullanimDeneme_1;
+
+public class ozellik1 {
+    public static void main(String[] args) {
+        System.out.println("Altay branch inde calısıyorum");
+    }
+}
